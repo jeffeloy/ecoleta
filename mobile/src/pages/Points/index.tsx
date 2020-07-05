cd91c84dba765e9bc4dfe326e9e61c01ee8ec657
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-07-04 21:04:51 -0300

    :rocket: Adiciona mapa na página Points

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Feather as Icon} from "@expo/vector-icons";
-import { Container, Title, Description, TouchbaleIcon } from "./styles";
+import { 
+  Container, 
+  Title, 
+  Description, 
+  TouchbaleIcon, 
+  MapContainer, 
+  ItemsContainer, 
+  Item, 
+  ItemTitle,
+  styles} from "./styles";
+import {ScrollView} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import MapView, {Marker} from "react-native-maps";
+import Emoji from "react-native-emoji";
+import {SvgUri} from "react-native-svg";
 
 const Points = () => {
   const navigation = useNavigation();
@@ -12,14 +24,68 @@ const Points = () => {
   }
 
   return (
-    <Container>
-      <TouchbaleIcon onPress={handleNavigateBack}>
-        <Icon name="arrow-left" size={20} color="#34cb79"/>
-      </TouchbaleIcon>
+    <>
+      <Container>
+        <TouchbaleIcon onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79"/>
+        </TouchbaleIcon>
 
-      <Title>Bem vindo.</Title>
-      <Description>Encontre no mapa um ponto de coleta.</Description>
-    </Container>
+        <Title>
+        <Emoji name="smile" style={{fontSize: 20}}/>
+          Bem vindo.
+        </Title>
+        <Description>Encontre no mapa um ponto de coleta.</Description>
+
+        <MapContainer>
+          <MapView 
+            style={styles.map}
+            initialRegion={{
+              latitude: -12.9545284,
+              longitude: -38.6335703,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+
+          </MapView>
+        </MapContainer>
+      </Container>
+      <ItemsContainer>
+        <ScrollView 
+          horizontal 
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{paddingHorizontal: 20 }}
+        >
+          <Item onPress={() =>{}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg" />
+            <ItemTitle>Lâmpadas</ItemTitle>
+          </Item>
+          <Item onPress={() =>{}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg" />
+            <ItemTitle>Lâmpadas</ItemTitle>
+          </Item>
+          <Item onPress={() =>{}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg" />
+            <ItemTitle>Lâmpadas</ItemTitle>
+          </Item>
+          <Item onPress={() =>{}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg" />
+            <ItemTitle>Lâmpadas</ItemTitle>
+          </Item>
+          <Item onPress={() =>{}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg" />
+            <ItemTitle>Lâmpadas</ItemTitle>
+          </Item>
+          <Item onPress={() =>{}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.103:3333/uploads/lampadas.svg" />
+            <ItemTitle>Lâmpadas</ItemTitle>
+          </Item>
+        </ScrollView>
+      </ItemsContainer>
+
+    
+    </>
   );
 };
+
 export default Points;
--- mobile/src/pages/Points/styles.tsx
+++ mobile/src/pages/Points/styles.tsx
@@ -1,13 +1,15 @@
 import styled from "styled-components/native";
-import Constants from "expo-constants";
+import {StyleSheet} from "react-native";
 
 export const Container = styled.View`
   flex: 1;
   padding: 32px;
-  padding-top: 20px;
+  padding-top: 40px;
 `;
 export const Title = styled.Text`
-  color: #000;
+  font-size: 20px;
+  font-family: 'Ubuntu_700Bold';
+  margin-top: 24px;
 `;
 export const Description = styled.Text`
   color:#6c6c80;
@@ -16,3 +18,42 @@ export const Description = styled.Text`
   font-family: 'Roboto_400Regular';
 `;
 export const TouchbaleIcon = styled.TouchableOpacity``;
+export const MapContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+  margin-top: 16px;
+`;
+export const ItemsContainer = styled.View`
+  flex-direction: row;
+  margin-top: 16px;
+  margin-bottom: 32px;
+`;
+export const Item = styled.TouchableOpacity`
+  background-color: #fff;
+  border-width: 2px;
+  border-color: #eee;
+  height: 120px;
+  width: 120px;
+  border-radius: 8px;
+  padding: 16px;
+  padding-top: 20px;
+  padding-bottom: 16px;
+  margin-right: 8px;
+  align-items: center;
+  justify-content: space-between;
+
+  text-align: center;
+`;
+export const ItemTitle = styled.Text`
+  font-family: 'Roboto_400Regular';
+  text-align: center;
+  font-size: 13px;
+`;
+export  const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+})
