56b0fd3fc86a79606c7a9dd937c4658a0962b305
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-07-19 23:25:20 -0300

    :rocket: Adiciona dropzone para upload de imagens

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -8,7 +8,7 @@ import { LeafletMouseEvent } from "leaflet";
 import api from "../../services/api";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
-
+import Dropzone from "../../components/Dropzone";
 // Sempre que criar um estado
 //pra um array ou objeto: informar manualmente o tipo da variável
 
@@ -46,6 +46,7 @@ const CreatePoint = () => {
     0,
     0,
   ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -124,16 +125,23 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+  
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+
+      if(selectedFile) {
+
+        data.append('image', selectedFile);
+      }
+    
 
     await api.post("points", data);
 
@@ -155,6 +163,8 @@ const CreatePoint = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile}/>
 
         <fieldset>
           <legend>
