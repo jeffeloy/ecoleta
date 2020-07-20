4f9db11b8d3cc8bd022d55fbe05ae1061f25c8d9
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-07-20 09:57:00 -0300

    :lock: Adiciona validação na rota de criação do ponto de coleta

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import routes from "./routes";
 import path from "path";
-
+import routes from "./routes";
+import { errors } from "celebrate";
 const app = express();
 
 app.use(cors());
@@ -10,5 +10,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(3333);
